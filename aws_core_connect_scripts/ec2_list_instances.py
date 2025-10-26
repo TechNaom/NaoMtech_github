cd36639fa1d78902fb5e918c986f7accf63c2c2c
Rudra-Papasani <technaom@gmail.com>
2025-10-26 11:26:54 +0530

    Created new project for AWS Automation.

--- aws_core_connect_scripts/ec2_list_instances.py
+++ aws_core_connect_scripts/ec2_list_instances.py
@@ -1,31 +1,19 @@
 import boto3
 
-ec2 = boto3.client('ec2')
-
+ec2 = boto3.client('ec2', region_name='ap-south-1')
 response = ec2.describe_instances()
 
-print("EC2 Instances:")
-for reservation in response['Reservations']:
-    for instance in reservation['Instances']:
-        instance_id = instance['InstanceId']
-        state = instance['State']['Name']
-        # Get Name tag if exists
-        #name_tag = next((tag['Value'] for tag in instance.get('Tags', []) if tag['Key'] == 'Name'), None)
-        #print(f"- ID: {instance_id}, State: {state}, Name: {name_tag}")
-        # Get the tags for this instance (if any)
-        tags = instance.get('Tags', [])
+if not response['Reservations']:
+    print("No EC2 instances found in this region.")
+else:
+    for reservation in response['Reservations']:
+        for instance in reservation['Instances']:
+            instance_id = instance['InstanceId']
+            state = instance['State']['Name']
+            tags = instance.get('Tags', [])
+            name_tag = next((tag['Value'] for tag in tags if tag['Key'] == 'Name'), "No Name")
 
-        # Default name if no tag found
-        name_tag = "No Name"
-
-        # Loop through all tags and find the one with Key = 'Name'
-        for tag in tags:
-            if tag['Key'] == 'Name':
-                name_tag = tag['Value']
-
-        # Print instance details clearly
-        print(f"Instance ID: {instance_id}")
-        print(f"State: {state}")
-        print(f"Name: {name_tag}")
-        print("-" * 30)  # line separator for readability
-
+            print(f"Instance ID: {instance_id}")
+            print(f"State: {state}")
+            print(f"Name: {name_tag}")
+            print("-" * 30)
